b131e309101b916fda2a24eb40f0442e7d588043
Purpkin <guiejo2020@gmail.com>
2025-02-26 23:41:13 -0300

    Basic player gun aiming

--- scripts/player/player_animations.gd
+++ scripts/player/player_animations.gd
@@ -2,6 +2,9 @@ extends AnimatedSprite2D
 class_name PlayerAnimations
 
 func play_anim(name: StringName):
-    if animation == name:
-        return
-    play(name)
+	if animation == name:
+		return
+	play(name)
+	
+func _process(delta: float) -> void:
+	flip_h = get_local_mouse_position().x < 0

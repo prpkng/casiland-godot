de8ac346162e798c82c4c7933fa9ef26c39f87d4
Purpkin <guiejo2020@gmail.com>
2025-02-28 20:23:03 -0300

    Pool ball animation

---
+++ scripts/entities/snooker/ball_sprite.gd
@@ -0,0 +1,21 @@
+extends AnimatedSprite2D
+
+@export var ball_rb: RigidBody2D
+
+const SPEED_MULTI = .05
+const SPEED_POW = .75
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _physics_process(delta: float) -> void:
+	var dir = ball_rb.linear_velocity.normalized()
+	var angle = atan2(dir.y, dir.x)
+	#rotation = lerp_angle(rotation, angle, 0.5)
+	rotation = angle
+	#rotation_degrees = deg
+	
+	speed_scale = (ball_rb.linear_velocity.length() * SPEED_MULTI) ** SPEED_POW
+	print(speed_scale)
+	

f36fa853bb733a0fd6bbd11fa5e4c8ed6cbf6992
prpkng <68878717+prpkng@users.noreply.github.com>
2025-05-21 22:53:33 +0000

    Deploying to gh-pages from @ prpkng/casiland-godot@5b5058f11d55a9edb32563fac5f7f5e7c4d77b01 🚀

--- dev/index.service.worker.js
+++ dev/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1744943946|8043973';
+const CACHE_VERSION = '1747868002|8209685';
 /** @type {string} */
 const CACHE_PREFIX = 'casiland-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

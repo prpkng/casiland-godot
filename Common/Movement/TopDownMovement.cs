74d1dc5195a7fc6044cacc56a95ea917e39588f4
Prpkng <guiejo2020@gmail.com>
2026-01-09 12:10:16 -0300

    chore: basic player fsm setup wip

---
+++ Common/Movement/TopDownMovement.cs
@@ -0,0 +1,62 @@
+﻿using Godot;
+using Godot.Collections;
+using TriangleNet;
+using UnityHFSM;
+using Log = Serilog.Log;
+
+namespace Casiland.Common.Movement;
+
+[GlobalClass]
+public partial class TopDownMovement : Node
+{
+    [Export] public CharacterBody2D CharacterBody
+    {
+        get => _body;
+        set
+        {
+            _body = value;
+            UpdateConfigurationWarnings();
+        }
+    }
+    private CharacterBody2D _body;
+
+    [ExportGroup("Movement", "movement")]
+    [Export] public float MovementSpeed;
+    [Export] public float MovementAcceleration;
+    [Export] public float MovementDeceleration;
+
+    public Vector2 MovementInput;
+
+    public override string[] _GetConfigurationWarnings()
+    {
+        if (CharacterBody == null)
+            return ["TopDownMovement needs a ChracterBody2D to work properly"];
+        return [];
+    }
+
+    private StateMachine _fsm;
+    
+    public override void _Ready()
+    {
+        _fsm = new StateMachine();
+        
+        _fsm.AddState("idle", null, OnIdle);
+        _fsm.AddState("move", null, OnMove);
+        
+        _fsm.Init();
+    }
+
+    private void OnIdle(State<string, string> state)
+    {
+        Log.Information("idle");
+    }
+    private void OnMove(State<string, string> state)
+    {
+        
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        _fsm.OnLogic();
+    }
+}
---
+++ Entities/Player/Player.cs
@@ -0,0 +1,20 @@
+﻿using Casiland.Common.Movement;
+using Godot;
+
+namespace Casiland.Entities.Player;
+
+[GlobalClass]
+public partial class Player : CharacterBody2D
+{
+    [Export] public TopDownMovement Movement;
+
+    public override void _Process(double delta)
+    {
+        var input = new Vector2
+        {
+            X = Input.GetAxis("move_left", "move_right"),
+            Y = Input.GetAxis("move_up", "move_down")
+        }.Normalized();
+        Movement.MovementInput = input;
+    }
+}
